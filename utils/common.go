305183205b7770d8991243258411057b981b1b0a
Duong Son <duongson212998@gmail.com>
2024-08-14 11:06:47 +0700

    feat: add metadata grpc

--- utils/common.go
+++ utils/common.go
@@ -1,10 +1,26 @@
 package utils
 
-import "github.com/jutimi/grpc-service/common"
+import (
+	"time"
+
+	"github.com/jutimi/grpc-service/common"
+	"google.golang.org/grpc/metadata"
+)
 
 func FormatErrorResponse(errCode int32, errMessage string) *common.ErrorResponse {
 	return &common.ErrorResponse{
 		ErrorCode:    errCode,
 		ErrorMessage: errMessage,
 	}
+}
+
+type Metadata struct {
+	ServiceName string
+}
+
+func GenerateMetaData(data Metadata) metadata.MD {
+	return metadata.Pairs(
+		"time", time.Now().Format("2006-01-02 15:04:05"),
+		"service_name", data.ServiceName,
+	)
 }
